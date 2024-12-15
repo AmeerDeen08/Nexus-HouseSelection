5f37ca6aa7d4ba02fa875cea3616e420047a2f2e
Ameer <ameerdeen592@gmail.com>
2024-12-15 20:49:04 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { StudentProvider } from "./components/StudentContext";
 import StudentForm from "./components/StudentForm";
 
@@ -7,11 +6,11 @@ import StudentForm from "./components/StudentForm";
 const App = () => {
   return (
     <StudentProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<StudentForm />} />
-        </Routes>
-      </Router>
+
+
+     <StudentForm />
+
+
     </StudentProvider>
   );
 };
