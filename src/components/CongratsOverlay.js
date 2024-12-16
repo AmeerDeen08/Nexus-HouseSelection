a9711df457e16894ce5f20311b5998f11023641e
Ameer <ameerdeen592@gmail.com>
2024-12-16 13:33:51 +0530

    added social link

--- src/components/CongratsOverlay.js
+++ src/components/CongratsOverlay.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import '../styles/CongratsOverlay.css';
-
+import Social from '../assets/images/Social.jpg'
 const CongratsOverlay = ({ house }) => {
   return (
     <div className="overlay">
       <div className="overlay-content">
         <h2>Congrats! Your House is</h2>
-        <h2 className='text-success'><strong>{house}</strong></h2>
+        <h2 className='text-success'><strong>{house.toUpperCase()}</strong></h2>
+        <p className='mt-3'>For More Info and Updates</p>
+      
+        <a href="https://www.instagram.com/nexus_hits/profilecard/?igsh=MXM5NHk4N20xeXRkOA==" target="_blank" rel="noopener noreferrer">
+<img src={Social} className='social socialcon' alt='socialicon'/></a></div>
       </div>
-    </div>
+    
   );
 };
 
